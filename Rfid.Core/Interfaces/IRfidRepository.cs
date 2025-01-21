39ca7aef2e34c81cbc5aca280bafabfaa12ea52d
Jonas Nygaard Engedal <jen@cvation.com>
2025-01-21 12:11:00 +0100

    Comment

--- Rfid.Core/Interfaces/IRfidRepository.cs
+++ Rfid.Core/Interfaces/IRfidRepository.cs
@@ -1,4 +1,6 @@
 ﻿namespace Rfid.Core.Interfaces;
+// This could instead be a generic repository like the one from Ardalis.Specification but e.g. placed in Inc.Persistence.Abstractions
+// https://github.com/ardalis/Specification/blob/main/Specification/src/Ardalis.Specification/IRepositoryBase.cs
 public interface IRfidRepository
 {
     Task<Entities.Rfid> GetAsync(Guid id);
