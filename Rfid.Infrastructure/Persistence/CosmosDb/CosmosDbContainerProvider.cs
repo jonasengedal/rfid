60c30ddb2bd5a4a6609fb5ce283d17edde18c7dd
Jonas Nygaard Engedal <jen@cvation.com>
2025-01-21 07:47:00 +0100

    Comment

--- Rfid.Infrastructure/Persistence/CosmosDb/CosmosDbContainerProvider.cs
+++ Rfid.Infrastructure/Persistence/CosmosDb/CosmosDbContainerProvider.cs
@@ -6,6 +6,7 @@ public class CosmosDbContainerProvider
 {
     internal async Task<Container> GetContainer()
     {
+        // TODO: Do not hard code read from IConfiguration or IOptions
         CosmosClient client = new(
             accountEndpoint: "https://localhost:8081/",
             authKeyOrResourceToken: "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
